1d91187a110c391655af4a690f2337b4d28cab3d
Peter Waller <peter@scraperwiki.com>
2014-06-10 16:29:15 +0100

    Cleanup

--- main.go
+++ main.go
@@ -7,20 +7,11 @@ import (
 	"log"
 	"os"
 	"runtime"
-	// "regexp"
 	"sync"
 	"time"
 
 	"github.com/edsrzf/mmap-go"
-	// "github.com/moovweb/rubex"
-	// "code.google.com/p/ahocorasick"
 )
-
-const MiB = 1024 * 1024
-
-func N(n int) []struct{} {
-	return make([]struct{}, n)
-}
 
 func Index(s, sep []byte, start int) int {
 	i := bytes.Index(s[start:], sep)
@@ -70,9 +61,6 @@ func main() {
 
 	result := make(chan [][]byte)
 
-	// regex := rubex.MustCompile("(?i)beckham")
-	// _ = regex
-
 	readChunk := func(idx int) {
 		start, end := idx*chunkSize, (idx+1)*chunkSize
 		if idx == Nreaders-1 {
@@ -80,28 +68,6 @@ func main() {
 		}
 
 		data := mapping[start:end]
-		// wholeData := mapping[start:end]
-
-		// var n int
-		// for {
-		// 	n++
-		// 	i := bytes.Index(data, []byte("eckham"))
-		// 	if i == -1 {
-		// 		break
-		// 	}
-		// 	nextNL := bytes.IndexAny(data, "\n")
-		// 	here := len(wholeData) - len(data) + i
-		// 	dataUptoHere := wholeData[:here]
-		// 	prevNL := bytes.LastIndexAny(dataUptoHere, "\n")
-		// 	if prevNL == -1 {
-		// 		prevNL = 0
-		// 	}
-		// 	// log.Println("i=", i, string(data[i:i+50]))
-		// 	log.Println("start, end, len =", prevNL, here-i+nextNL, len(wholeData)-(here-i+nextNL))
-		// 	line := wholeData[prevNL : here-i+nextNL]
-		// 	fmt.Fprintln(os.Stdout, line)
-		// 	data = data[i+1:]
-		// }
 
 		i := -1
 		var n int
@@ -117,57 +83,14 @@ func main() {
 			nextNL := NextNewline(data, i)
 			if nextNL == -1 {
 				// Line end is at end of chunk
-				// nextNL = len(data) - 1
 				break
 			}
 
 			i = nextNL
-			// prevNL := bytes.LastIndexAny(data[:i], "\n")
-			// if prevNL == -1 {
-			// 	// Line start is at beginning of chunk
-			// 	prevNL = 0
-			// }
-			// nextNL := bytes.IndexAny(data[i:], "\n")
-			// if nextNL == -1 {
-			// 	// Line end is at end of chunk
-			// 	nextNL = len(data) - 1
-			// } else {
-			// 	nextNL += i
-			// }
-
 			line := data[prevNL:nextNL]
 			localResult = append(localResult, line)
 		}
 		result <- localResult
-		log.Println("count =", n)
-
-		// needle := ahocorasick.NewAhoCorasick([]string{"eckham"})
-
-		// var n int
-		// for result := range ahocorasick.MatchBytes(data, needle) {
-		// 	n++
-		// 	_ = result
-		// }
-		// result <- n
-		// log.Println("Here:", len(data))
-		// locs := regex.FindIndex(data[:100*1024])
-		// matches := len(locs)
-		// result <- matches
-
-		// buf := bytes.NewReader(data)
-		// s := bufio.NewScanner(buf)
-
-		// lines := 0
-		// matches := 0
-
-		// for s.Scan() {
-		// 	lines++
-		// 	locs := regex.FindIndex(s.Bytes())
-		// 	matches += len(locs)
-		// }
-		// log.Println("Read", lines, "lines")
-
-		// result <- matches
 	}
 
 	var wg sync.WaitGroup
