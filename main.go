ab89a03bf9e49ae7a8401a65c71e1a4809056aef
Peter Waller <peter@scraperwiki.com>
2014-06-10 10:26:57 +0100

    Experiments in mmapping

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/edsrzf/mmap-go"
+)
+
+func main() {
+	fd, err := os.Open("/mem/output-xsd-fix-no-provenance-factuality.tql")
+	if err != nil {
+		panic(err)
+	}
+	defer fd.Close()
+	mapping, err := mmap.Map(fd, mmap.RDONLY, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	var b byte
+	amount := 100 * 1024 * 1024
+
+	for _, v := range mapping[:amount] {
+		b += v
+	}
+	log.Println("Result =", b)
+}
