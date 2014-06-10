ca73690c56a3c27baa2756255ec6929357a905ec
Peter Waller <peter@scraperwiki.com>
2014-06-10 12:04:37 +0100

    Also compute sum(bytes) in serial

--- main.go
+++ main.go
@@ -76,13 +76,13 @@ func main() {
 	close(result)
 	<-finished
 
-	// var b byte
 	// amount := 10 * 1024 * 1024
 
-	// for _, v := range mapping[:amount] {
-	// 	b += v
-	// }
-	// log.Println("Result =", b)
+	var b byte
+	for _, v := range mapping {
+		b += v
+	}
+	log.Println("Result =", b)
 	// data := mapping[:amount]
 
 	// start := time.Now()
